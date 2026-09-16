7fe9ae92cc7749451cb73eec482cb03d30717530
carlinbrr <carloscollaguazo5@gmail.com>
2026-09-16 13:43:52 +0200

    refactor: update html layout definition

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
 import { NavBar } from "./shared/nav-bar/nav-bar";
 import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, Footer],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [Header, NavBar, RouterOutlet, Footer],
+  templateUrl: './app.html'
 })
 export class App {}
